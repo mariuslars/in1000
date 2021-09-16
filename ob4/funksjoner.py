fc7cdbc2fcbbf105a0c3d393197f0b6d6c04f43e
Larsson <riu@ssb.no>
2021-09-16 08:25:30 +0200

    iterating over string instead of index

--- ob4/funksjoner.py
+++ ob4/funksjoner.py
@@ -24,10 +24,10 @@ def tellForekomst(minTekst, minBokstav):
 
     #Initiate letter counter. Will increase by one each time the letter is in the sentence
     point_counter = 0
-    for letter in range(len(minTekst)):
-
+    #for letter in range(len(minTekst)):
+    for letter in minTekst:
     #Hvis den oppgitte bokstaven er i strengen, pluss på én i counter
-        if(minBokstav in minTekst[letter]):
+        if(minBokstav in letter):
             point_counter += 1
     
     return point_counter
@@ -37,11 +37,13 @@ def tellForekomst(minTekst, minBokstav):
 input_string = input("Skriv inn en tekst: ")
 input_letter = input("Skriv inn én bokstav: ")
 
-required_letter_length = 1
+
 
 #Er dette litt for voldsomt å kaste error for? Burde man heller ha en while loop som går så lenge len(input_letter) != 1?
+#required_letter_length = 1
 #assert len(input_letter) == required_letter_length, "antall bokstaver er for mange. Skriv kun én bokstav"
 
 antall_forekomster = tellForekomst(input_string, input_letter)
     
 print(f"I tekststrengen '{input_string}' forekommer bokstaven '{input_letter}' {antall_forekomster} ganger")
+
