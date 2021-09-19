209cb5867a4b3d8c1002f52a6433c9565935652f
marius <marius.runningen.larsson@ssb.no>
2021-09-19 09:33:31 +0200

    improved solution for regneklokke.py by removing the nested for loop and kept only the single

--- ob4/regneklokke.py
+++ ob4/regneklokke.py
@@ -41,25 +41,18 @@ print("Sum av dine tall er ", minSum)
 #minimum_value må inneholde en initiell verdi for å kunne evalueres mot de andre i listen.
 #Kan ikke initieres med '0' da dette vil påvirke hva som blir minimum hvis bruker taster inn tall i intervallet (0, Inf)
 #Følgelig kan den initieres med en vilkårlig verdi som er oppgitt av bruker
-minimum_value = user_input_list[0]
+maximum_value = minimum_value = user_input_list[0]
 
-#Nested for loop som først henter ut tallet som skal bli evaluert. Så evaluerer den om det tallet er mindre enn alle andre i listen
-#Evaluerer også om tallet er mindre enn hva som for øyeblikket er det laveste tallet
-#Kravet for at tallet for evaluering er dermed: må både være mindre enn tallet det blir sammenlignet med OG hva som foreløpig er det minste tallet som er evaluert
+#For loop som først henter ut tallet som skal bli evaluert. 
+#Evaluerer den om det tallet er mindre eller større en hva som ble assignet til min_val og max_val
 for number_for_evaluation in user_input_list:
-    for number in user_input_list:
-        if((number_for_evaluation < number) & (number_for_evaluation < minimum_value)):
-            minimum_value = number_for_evaluation
+    #for number in user_input_list:
+    if(number_for_evaluation < minimum_value):
+        minimum_value = number_for_evaluation
+        
+    if(number_for_evaluation > maximum_value):
+        maximum_value = number_for_evaluation
 
 
 print("minste verdi av dine tall er: ", minimum_value)
-
-#Identisk kode og prinsipp som for minimum, men her er kravet at number_for_evlauation må være størren enn både nummeret og maximum_value
-maximum_value = user_input_list[0]
-for number_for_evaluation in user_input_list:
-    for number in user_input_list:
-        if((number_for_evaluation > number) & (number_for_evaluation > maximum_value)):
-            maximum_value = number_for_evaluation
-
-
 print("største verdi av dine tall er: ", maximum_value)
