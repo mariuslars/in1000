511eb733a0bdf73a044f1933c613dbb18578e2ae
marius <marius.runningen.larsson@ssb.no>
2021-10-09 18:10:25 +0200

    further updates to ob6

--- ob6/testDato.py
+++ ob6/testDato.py
@@ -2,7 +2,7 @@ from dato import Dato
 
 def hovedprogram():
 
-    femtende = Dato(5, 10, 2022)
+    femtende = Dato(15, 10, 2021)
 
     print(femtende.lesAar())
     
@@ -15,8 +15,32 @@ def hovedprogram():
     
     print(lesbar_dato)
 
-    nyDato = Dato(15, 10, 2023)
+    nyDato1 = Dato(15, 11, 2021)
+    nyDato2 = Dato(15, 5, 2021)
+    nyDato3 = Dato(15, 10, 2021)
+    nyDato4 = Dato(16, 10, 2021)
+    nyDato5 = Dato(14, 10, 2021)
 
-    femtende.sjekkRekkefolgeDato(nyDato)
+    gammelString = "gammel dato er: " + femtende.lagFinDato()
+    
+    print(gammelString)
+    print("ny dato er: ", nyDato1.lagFinDato())
+    femtende.sjekkRekkefolgeDato(nyDato1)
+
+    print(gammelString)
+    print("ny dato er: ", nyDato2.lagFinDato())
+    femtende.sjekkRekkefolgeDato(nyDato2)
+
+    print(gammelString)
+    print("ny dato er: ", nyDato3.lagFinDato())
+    femtende.sjekkRekkefolgeDato(nyDato3)
+
+    print(gammelString)
+    print("ny dato er: ", nyDato4.lagFinDato())
+    femtende.sjekkRekkefolgeDato(nyDato4)
+
+    print(gammelString)
+    print("ny dato er: ", nyDato5.lagFinDato())
+    femtende.sjekkRekkefolgeDato(nyDato5)
 
 hovedprogram()
