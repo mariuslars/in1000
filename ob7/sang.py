65ba8134ba13f5af51d3f9a72be8b5c2b10653ae
marius <marius.runningen.larsson@ssb.no>
2021-10-16 00:10:51 +0200

    before refactor. Remove str form finnSang if everything fails

--- ob7/sang.py
+++ ob7/sang.py
@@ -1,18 +1,32 @@
 class Sang:
 
     def __init__(self, artist, tittel):
+        
         self._artist = artist
         self._tittel = tittel
+
+    def __str__(self):
+
+        return f'{self._tittel};{self._artist}'
 
     def spill(self):
 
         print(f"Spiller {self._tittel} av {self._artist}")
 
     def sjekkArtist(self, navn):
-
+        #Splitter opp og lager sett siden det holder med partial match på ett eller flere ord
         artist_split = self._artist.lower().split(" ")
+        artistnavn_split = navn.lower().split(" ")
 
-        if navn.lower() in artist_split:
+        artist_set = set(artist_split)
+        artistnavn_set = set(artistnavn_split)
+
+        #Finner intersection av settene for å senere vurdere lengden. Hvis den er 0 er det ingen match
+        artist_intersection = artist_set.intersection(artistnavn_set)
+
+        set_is_not_empty = len(artist_intersection) > 0
+
+        if set_is_not_empty:
 
             return True
 
@@ -30,6 +44,7 @@ class Sang:
             return False
 
     def sjekkArtistOgTittel(self, artist, tittel):
+        
 
         both_are_true = self.sjekkArtist(artist) & self.sjekkTittel(tittel)
 
@@ -41,9 +56,7 @@ class Sang:
 
             return False
         
-    def __str__(self):
 
-        return f'{self._tittel};{self._artist}'
 
 
 """ sang1 = Sang("Prince of arabia", "night of the storm")
--- ob7/spilleliste.py
+++ ob7/spilleliste.py
@@ -1,59 +1,93 @@
 from sang import Sang
 
 class Spilleliste:
+
     def __init__(self, listenavn):
         self._sanger = []
         self._navn = listenavn
 
     def __str__(self):
+        #Definerer str-metode mest for å kunne enkelt sjekke innholdet i objektet
+        sang_str_liste = []
+        for song in self._sanger:
+            #Benytter det faktum at vi har definert __str__-metode tidligere for enklere å parse sangen
+            sang_str_liste.append(str(song))
 
-        song_unpack = '|'.join([song for song in self._sanger])
-        return song_unpack
+        #Litt tilfeldig valg av print her. Men benytter | for å enkelt kunne splitte i fremtiden
+        return '|'.join(sang_str_liste)
+
+        
 
 
     def lesFraFil(self, filnavn):
 
+        #Åpner fil på standard måte
         open_music_file = open(filnavn)
 
         for line in open_music_file:
 
-            alleData = line.strip()#.split(";")
-            self._sanger.append(alleData)
+            alleData = line.strip().split(";")
+            #Legger til Sang objekter i spillelisten
+            self._sanger.append(Sang(alleData[1], alleData[0]))
 
     def leggTilSang(self, nySang):
 
         
-        nySang_entry = str(nySang)#.split(";")
-        self._sanger.append(nySang_entry)
+        self._sanger.append(nySang)
 
     def fjernSang(self, sang):
-        self._sanger = [liste_sang for liste_sang in self._sanger if sang not in liste_sang]
 
+        keep_sang_list = []
+
+        for liste_sang in self._sanger:
+            #I stedet for å fjerne sangen som skal ut gjør vi det heller sånn at vi beholder alle andre sanger
+            if str(sang) != str(liste_sang):
+
+                keep_sang_list.append(liste_sang)
+
+        self._sanger = keep_sang_list
+
+    def spillSang(self, sang):
+
+        
+        sang.spill()
 
     def spillAlle(self):
 
         for sang in self._sanger:
 
-            print(f"Spiller av: {sang}")
+            sang.spill()
 
     
     def finnSang(self, tittel):
 
         for sang in self._sanger:
-            splitta_sang = sang.split(";")
-            if tittel.lower() == splitta_sang[0].lower():
+            
+            if sang.sjekkTittel(tittel):
 
-                return(sang)
+                return(str(sang))
 
         return None
 
+
     def hentArtistUtvalg(self, artistnavn):
-            print()
+            
+            artist_liste_sanger = []
+            for sang in self._sanger:
+
+                if sang.sjekkArtist(artistnavn):
+
+                    artist_liste_sanger.append(str(sang))
+
+            return artist_liste_sanger
+
 
     
 
 tester = Spilleliste("Listenavn")
 tester.lesFraFil("musikk.txt")
+
+
 
 print("Print object: ")
 print(tester)
@@ -64,8 +98,11 @@ tester.leggTilSang(sang1)
 print("Printer etter å ha lagt til sang: ")
 print(tester)
 
-funnet_sang_ikke = tester.finnSang("GoiNG for the onasddase")
+
+print("-----------------------------------------------")
 print("Printer resultat av sang som ikke er funnet: ")
+funnet_sang_ikke = tester.finnSang("GoiNG for the oned")
+
 print(funnet_sang_ikke)
 funnet_sang = tester.finnSang("GoiNG for the one")
 print("Dette er sang som er funnet: ",funnet_sang)
@@ -76,3 +113,5 @@ tester.fjernSang(funnet_sang)
 print(tester)
 
 tester.spillAlle()
+print(Sang("Queen of", "LOL").sjekkArtist("qudeen of d"))
+print(tester.hentArtistUtvalg("iver"))
