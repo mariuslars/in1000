663bb69d21f02f6ddb4cf63ea7b255342166416e
marius <marius.runningen.larsson@ssb.no>
2021-10-15 23:11:26 +0200

    commit after refactor to Sang class

--- ob7/spilleliste.py
+++ ob7/spilleliste.py
@@ -7,7 +7,7 @@ class Spilleliste:
 
     def __str__(self):
 
-        song_unpack = '|'.join([song[0]+";"+song[1] for song in self._sanger])
+        song_unpack = '|'.join([song for song in self._sanger])
         return song_unpack
 
 
@@ -17,41 +17,62 @@ class Spilleliste:
 
         for line in open_music_file:
 
-            alleData = line.strip().split(";")
+            alleData = line.strip()#.split(";")
             self._sanger.append(alleData)
 
     def leggTilSang(self, nySang):
 
         
-        nySang_entry = str(nySang).split(";")
+        nySang_entry = str(nySang)#.split(";")
         self._sanger.append(nySang_entry)
 
     def fjernSang(self, sang):
-        [liste_sang for liste_sang in self._sanger if sang not in liste_sang]
+        self._sanger = [liste_sang for liste_sang in self._sanger if sang not in liste_sang]
 
-    def finnSang(self, tittel):
+
+    def spillAlle(self):
 
         for sang in self._sanger:
 
-            if tittel.lower() == sang[0].lower():
+            print(f"Spiller av: {sang}")
+
+    
+    def finnSang(self, tittel):
+
+        for sang in self._sanger:
+            splitta_sang = sang.split(";")
+            if tittel.lower() == splitta_sang[0].lower():
 
                 return(sang)
 
         return None
 
+    def hentArtistUtvalg(self, artistnavn):
+            print()
+
+    
+
 tester = Spilleliste("Listenavn")
 tester.lesFraFil("musikk.txt")
 
+print("Print object: ")
 print(tester)
+
 sang1 = Sang("Bon Iver", "The Great River")
 tester.leggTilSang(sang1)
 
+print("Printer etter å ha lagt til sang: ")
 print(tester)
-funnet_sang = tester.finnSang("GoiNG for the one")
-print("dette re funnet sang: ",funnet_sang)
 
+funnet_sang_ikke = tester.finnSang("GoiNG for the onasddase")
+print("Printer resultat av sang som ikke er funnet: ")
+print(funnet_sang_ikke)
+funnet_sang = tester.finnSang("GoiNG for the one")
+print("Dette er sang som er funnet: ",funnet_sang)
+
+print("Fjerner sangen over: ")
 tester.fjernSang(funnet_sang)
 
 print(tester)
 
-print(tester._sanger)
+tester.spillAlle()
